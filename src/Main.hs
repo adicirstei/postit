e8abc06f0b526b8090781df45d055ff14b462922
Adrian Cirstei <adicirstei@gmail.com>
2018-04-03 18:04:48 +0300

    IORef addition

--- src/Main.hs
+++ src/Main.hs
@@ -1,47 +1,95 @@
+{-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE OverloadedLabels  #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 
-import           Control.Monad          (void)
--- import           Control.Monad.Trans.Reader (runReaderT)
--- import           Data.IORef                 (newIORef, readIORef, writeIORef)
--- import           Data.Maybe                 (isJust)
+import           Control.Monad          (void, when)
 
 import           Control.Monad.IO.Class (MonadIO)
 import           Data.Foldable          (forM_)
 import           Data.GI.Base
+import           Data.Hashable          (Hashable (..))
+import           Data.HashMap.Lazy      (HashMap, fromList, lookupDefault)
 import           Data.Text              (Text)
 import           Data.Text.Encoding     (encodeUtf8)
 import           Data.Word              (Word32)
 
--- import qualified GI.Gdk                     as Gdk
--- import qualified GI.GdkPixbuf               as GP
--- import qualified GI.GLib                    as GLib
+import           Data.Monoid            ((<>))
+
 import qualified GI.Gtk                 as Gtk
 
+import           Data.IORef
+import           Debug.Trace
+import qualified GI.Gio                 as Gio
+
+
+data Msg
+  = NoteDeleted Int
+  | NoteUpdated Int WD
+
 type ColorSet = (Text, Text)
+data Style = Green | Yellow | Red | Purple | Gray
+  deriving (Eq)
+
+instance Hashable Style where
+  hashWithSalt s Green  = s
+  hashWithSalt s Yellow = s + 1
+  hashWithSalt s Red    = s + 2
+  hashWithSalt s Purple = s + 3
+  hashWithSalt s Gray   = s + 4
+
 
 data WD = WD
-  { color  :: Int
+  { id     :: Int
+  , color  :: Style
   , width  :: Int
   , height :: Int
   , text   :: Text
-  , css    :: Text
   }
 
 type Model = [ WD ]
 
+colorMap :: HashMap Style ColorSet
+colorMap = fromList [ (Green, ("rgb(137, 209, 189)", "rgb(32, 123, 53)"))
+                    , (Yellow, ("rgb(253, 222, 127)", "rgb(229, 90, 18)"))
+                    , (Red, ("rgb(251, 173, 113)", "rgb(203, 26, 25)"))
+                    , (Purple, ("rgb(141, 173, 209)", "rgb(115, 42, 139)"))
+                    , (Gray, ("rgb(175, 175, 175)", "rgb(65, 65, 65)"))
+                    ]
+
+w = WD 0 Yellow initialSize initialSize ""
+
+b = WD 1 Purple initialSize initialSize ""
+
+defaultColorSet :: ColorSet
+defaultColorSet = ("rgb(137, 209, 189)", "rgb(32, 123, 53)")
 
 
-w = WD 0 initialSize initialSize "" "textview.view text {background-color: rgb(137, 209, 189);}"
+onWindowClose :: IO ()
+onWindowClose = do
+  traceIO "buu"
+  Gtk.mainQuit
 
-b = WD 0 initialSize initialSize "" "textview.view text {background-color: rgb(174, 174, 212);}"
+
+styleCss :: Text -> Text
+styleCss color =
+  "*, * * {background-color: "
+  <> color
+  <> ";}"
+
+getStyleColorSet :: Style -> ColorSet
+getStyleColorSet s = lookupDefault defaultColorSet s colorMap
+
+
+getTitleColor :: Style -> Text
+getTitleColor = snd . getStyleColorSet
+getBodyColor = fst . getStyleColorSet
 
 
 initialModel :: Model
-initialModel = [w, b]
+initialModel = [w, b, WD 2 Gray initialSize initialSize "", WD 3 Red initialSize 400 ""]
 
 loadData :: MonadIO m => m Model
 loadData = do
@@ -51,26 +99,58 @@ loadData = do
 initialSize :: Int
 initialSize = 256
 
-createPostitWindow :: MonadIO m => WD -> m Gtk.Window
-createPostitWindow (WD c w h t s) = do
+createPostitWindow :: IORef Int -> WD -> IO Gtk.Window
+createPostitWindow ref (WD i s w h t) = do
   win <- new Gtk.Window [ #defaultWidth := fromIntegral w
                         , #defaultHeight := fromIntegral h
+                        , #decorated := False
                         ]
+
+  let bgCss = styleCss $ getBodyColor s
+      tlCss = styleCss $ getTitleColor s
+
 
   textBuf <- new Gtk.TextBuffer [ #text := t]
   textAr <- new Gtk.TextView [ #buffer := textBuf ]
   Gtk.textViewSetWrapMode textAr Gtk.WrapModeWordChar
   on textBuf #changed (set win [ #title := "miau" ])
-  on win #destroy Gtk.mainQuit
+  on win #destroy $ do
+    count <- atomicModifyIORef' ref (\c -> (c -1, c-1))
+    when (count <= 0) Gtk.mainQuit
+
+  buttAdd <- new Gtk.Button [ #label := "+" ]
+  buttColor <- new Gtk.Button [ #label := "..." ]
+  buttDel <- new Gtk.Button [ #label := "X" ]
+
+  on buttDel #clicked (#close win)
+  _ <- atomicModifyIORef' ref (\c -> (c + 1, ()))
+
+  box <- new Gtk.VBox []
+
+  toolbar <- new Gtk.Box []
 
 
-  #add win textAr
+  Gtk.boxPackStart toolbar buttAdd False False 0
+  Gtk.boxPackEnd toolbar buttDel False False 0
+  Gtk.boxPackEnd toolbar buttColor False False 0
+
+
+  Gtk.boxPackStart box toolbar False False 3
+  Gtk.boxPackStart box textAr True True 0
+
+  #add win box
 
   ctx <- Gtk.widgetGetStyleContext textAr
+  bgp <- Gtk.cssProviderNew
+  Gtk.cssProviderLoadFromData bgp (encodeUtf8 bgCss)
+  Gtk.styleContextAddProvider ctx bgp (fromIntegral Gtk.STYLE_PROVIDER_PRIORITY_USER :: Word32)
 
-  p <- Gtk.cssProviderNew
-  Gtk.cssProviderLoadFromData p (encodeUtf8 s)
-  Gtk.styleContextAddProvider ctx p (fromIntegral Gtk.STYLE_PROVIDER_PRIORITY_USER :: Word32)
+
+  tctx <- Gtk.widgetGetStyleContext toolbar
+  tlp <- Gtk.cssProviderNew
+  Gtk.cssProviderLoadFromData tlp (encodeUtf8 tlCss)
+  Gtk.styleContextAddProvider tctx tlp (fromIntegral Gtk.STYLE_PROVIDER_PRIORITY_USER :: Word32)
+
   return win
 
 
@@ -78,8 +158,11 @@ main :: IO ()
 main = do
   Gtk.init Nothing
 
+  app <- new Gtk.Application [ #flags := [ Gio.ApplicationFlagsFlagsNone ] ]
+  winCount <- newIORef 0
+
   d <- loadData
-  wins <- traverse createPostitWindow d
+  wins <- traverse (createPostitWindow winCount) d
 
   forM_ wins (#showAll)
 
